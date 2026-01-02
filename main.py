cfd078a4a13d91b236d28d644cccb5a713808c50
YaelIzhak <yaelizhak2001@gmail.com>
2026-01-02 14:27:23 +0200

    Update main.py
    6

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from skimage.morphology import ball
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = load_image('Timon')  
+image = load_image('Timon.jpg')
 clean_image = median(image, ball(3))  
 edgeMAG = edge_detection(clean_image)
-edge_binary = (edgeMAG > 50).astype(np.uint8) * 255
+edge_binary = edgeMAG > 50
 edge_image = Image.fromarray(edge_binary)
 edge_image.save('my_edges(1).png')
 
