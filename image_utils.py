85ee50e5fbd569dc21c0893221cc90f4b071d61e
YaelIzhak <yaelizhak2001@gmail.com>
2026-01-06 17:04:03 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -4,20 +4,28 @@ from scipy.signal import convolve2d
 
 def load_image(file_path):
     image = Image.open(file_path)
-    image = np.array(image) 
-    if image.ndim == 3:
-        image = image[..., 0]  
-    return image 
+    return np.array(image)
 
 def edge_detection(image):
-    gray_image = np.array(image)
+    if image.ndim == 3:
+        # חישוב ממוצע הערוצים להפיכה לאפור
+        gray_image = np.mean(image, axis=2)
+    else:
+        gray_image = image
+
+    # הגדרת קרנלים של Sobel
     kernelX = np.array([[-1, 0, 1],
                         [-2, 0, 2],
                         [-1, 0, 1]])
+    
     kernelY = np.array([[1, 2, 1],
                         [0, 0, 0],
                         [-1, -2, -1]])
+    
+    # ביצוע קונבולוציה (שימוש בייבוא ישיר מ-scipy)
     edgeX = convolve2d(gray_image, kernelX, mode='same', boundary='fill')
     edgeY = convolve2d(gray_image, kernelY, mode='same', boundary='fill')
+    
+    # חישוב עוצמת הקצוות
     edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
     return edgeMAG
